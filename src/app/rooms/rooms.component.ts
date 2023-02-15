d186163dc07549db3493bd029dd59748eff30a29
Anuja Sharma <anuja3580@gmail.com>
2023-02-15 14:55:50 -0600

    learning angular

---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,93 @@
+import { Component, DoCheck, OnChanges, OnInit } from '@angular/core';
+import { Room, RoomList } from './rooms';
+
+@Component({
+  selector: 'app-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.css']
+})
+export class RoomsComponent implements OnInit, DoCheck{
+ hotelName = 'Hilton hotel';
+ numberOfRooms = 10;
+ hideRooms = false;
+ selectedRoom !: RoomList;
+ title = 'Room List';
+
+
+ rooms: Room={
+  totalRooms : 20,
+  availableRooms: 10,
+  bookedRooms: 5
+ }
+
+ roomList: RoomList[]= [{
+  roomNumber: 234,
+  roomType: 'Deluxe Room',
+  amenities: 'Air Contdition, Free Wifi, Kitchen, TV, Bathroom',
+  price: 1000,
+  photos:"https://unsplash.com/s/photos/hotel-room" ,
+  checkInTime: new Date('11-Nov-2021'),
+  checkOutTime : new Date('12-Nov-2021'),
+  rating: 4.5
+ },
+ {
+  roomNumber: 234,
+  roomType: 'Deluxe Room',
+  amenities: 'Air Contdition, Free Wifi, Kitchen, TV, Bathroom',
+  price: 1000,
+  photos:"https://unsplash.com/s/photos/hotel-room" ,
+  checkInTime: new Date('11-Nov-2021'),
+  checkOutTime : new Date('12-Nov-2021'),
+  rating: 4.8
+ },
+ {
+  roomNumber: 234,
+  roomType: 'Deluxe Room',
+  amenities: 'Air Contdition, Free Wifi, Kitchen, TV, Bathroom',
+  price: 1000,
+  photos:"https://unsplash.com/s/photos/hotel-room" ,
+  checkInTime: new Date('11-Nov-2021'),
+  checkOutTime : new Date('12-Nov-2021'),
+  rating: 3.5
+ }]
+
+ constructor(){}
+
+  ngDoCheck(): void {
+    console.log('onchange is called')
+  }
+
+ ngOnInit(): void {
+   
+   
+ }
+
+ toggle(){
+  this.hideRooms= !this.hideRooms;
+  this.title = 'Rooms List';
+ }
+
+ selectRoom (room : RoomList){
+  this.selectedRoom = room;
+
+ }
+
+ addRoom(){
+  const room: RoomList = {
+    roomNumber : 4,
+    roomType : 'Deluxe Room',
+    amenities: 'Air Conditioner, Free Wifi, TV, bathroom, kitchen',
+    price : 500,
+    photos: "https://unsplash.com/s/photos/hotel-room",
+    checkInTime: new Date('11-Nov-2021'),
+    checkOutTime : new Date('12-Nov-2021'),
+    rating: 3.5
+
+
+  }
+  this.roomList= [...this.roomList, room];
+
+ }
+
+
+}
